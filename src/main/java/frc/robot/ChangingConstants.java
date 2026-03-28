410f1ee2c28f5bb13ce792a43c9974a35d6de64c
Will <willthemcer@gmail.com>
2026-03-28 13:43:25 -0400

    Constants setup

---
+++ src/main/java/frc/robot/ChangingConstants.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+
+/** Add your docs here. */
+public class ChangingConstants {
+    public static final class DriveConstants {
+        
+        public static final double kPTurning = 0.5; //should be good to go 
+
+        public static final double kFLabsEncoderOffsetRad = -0.254;
+        public static final double kFRabsEncoderOffsetRad = -1.252;
+        public static final double kBLabsEncoderOffsetRad = -1.816;
+        public static final double kBRabsEncoderOffsetRad = -4.811;
+        
+        public static final double kPhysicalMaxSpeedMetersPerSecond = 5;
+        public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 2 * 2 * Math.PI;
+        
+        //Teleop speeds:
+        public static final double kTeleDriveMaxSpeedMetersPerSecond = 
+                                    kPhysicalMaxSpeedMetersPerSecond / 4;
+
+        public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond =
+                                    kPhysicalMaxAngularSpeedRadiansPerSecond / 4;
+
+        public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 3;
+        public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = 3;
+    }
+    public static final class AutoConstants {
+        public static final double kMaxSpeedMetersPerSecond = 
+                                    DriveConstants.kPhysicalMaxSpeedMetersPerSecond / 4;
+        public static final double kMaxAngularSpeedRadiansPerSecond = 
+                                    DriveConstants.kPhysicalMaxAngularSpeedRadiansPerSecond / 10;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxAngularAccelerationRadiansPerSecondSquared = 
+                                    Math.PI / 4;
+        public static final double kPXController = 1.5;
+        public static final double kPYController = 1.5;
+        public static final double kPThetaController = 3;
+
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+                new TrapezoidProfile.Constraints(
+                        kMaxAngularSpeedRadiansPerSecond,
+                        kMaxAngularAccelerationRadiansPerSecondSquared);
+    }
+}
---
+++ src/main/java/frc/robot/StaticConstants.java
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class StaticConstants {
+  public static class OperatorConstants { //TODO: fill in
+    public static final int kDriverControllerPort = 0;
+    public static final int kDriverYAxis = 0;
+    public static final int kDriverXAxis = 0;
+    public static final int kDriverRotAxis = 0;
+    public static final int kDriverFieldOrientedButtonIdx = 0;
+    public static final double kDeadband = 0.05;
+  }
+  
+  public static final class ModuleConstants { 
+    public static final double kTrackWidth = Units.inchesToMeters(10.0);
+        // Distance between right and left wheels                TODO: fill in
+    public static final double kWheelBase = Units.inchesToMeters(10.0);
+        // Distance between front and back wheels                TODO: fill in
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+      new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+      new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+      new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
+      new Translation2d(-kWheelBase / 2, kTrackWidth / 2));
+
+      public static final class PhysicalConstants { //TODO: fill in
+        public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+        public static final double kDriveMotorGearRatio = 1 / 5.8462; //find on SDS website
+        public static final double kTurningMotorGearRatio = 1 / 18.0; //find on SDS website
+        public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+        public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+        public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+        public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+        public static final double kPTurning = ChangingConstants.DriveConstants.kPTurning;
+      }
+  }
+
+  public static final class DriveConstants {
+
+    public static final class CanIDs {
+      // Front Left Module TODO: fill in
+      public static final int kFLDrivePort = 0;
+      public static final boolean kFLDriveEncoderReversed = false;
+      public static final int kFLSteerPort = 0;
+      public static final boolean kFLSteerEncoderReversed = false;
+      public static final int kFLabsEncoderPort = 0;
+      public static final boolean kFLabsEncoderReversed = false;
+
+      // Front Right Module TODO: fill in
+      public static final int kFRDrivePort = 0;
+      public static final boolean kFRDriveEncoderReversed = false;
+      public static final int kFRSteerPort = 0;
+      public static final boolean kFRSteerEncoderReversed = false;
+      public static final int kFRabsEncoderPort = 0;
+      public static final boolean kFRabsEncoderReversed = false;
+
+      // Back Left Module TODO: fill in
+      public static final int kBLDrivePort = 0;
+      public static final boolean kBLDriveEncoderReversed = false;
+      public static final int kBLSteerPort = 0;
+      public static final boolean kBLSteerEncoderReversed = false;
+      public static final int kBLabsEncoderPort = 0;
+      public static final boolean kBLabsEncoderReversed = false;
+
+      // Back Right Module TODO: fill in
+      public static final int kBRDrivePort = 0;
+      public static final boolean kBRDriveEncoderReversed = false;
+      public static final int kBRSteerPort = 0;
+      public static final boolean kBRSteerEncoderReversed = false;
+      public static final int kBRabsEncoderPort = 0;
+      public static final boolean kBRabsEncoderReversed = false;
+
+
+    }
+  }
+}
